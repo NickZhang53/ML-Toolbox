be720a9202aff75ea75c82dbb1b1a24bdd087219
Yongxing Zhang <nickz@Yongxings-MacBook-Pro.local>
2022-10-16 15:21:45 -0400

    [DEBUG] Shape mismatch

--- LeNet.py
+++ LeNet.py
@@ -6,20 +6,20 @@ class LeNet(tf.keras.Model):
 
     def _feature_extraction(self, x):
         x = tf.keras.layers.Conv2D(
-            filters=6, kernel_size=(5, 5), activation='sigmoid'
+            filters=6, kernel_size=(5, 5), activation="sigmoid"
         )(x)
         x = tf.keras.layers.AveragePooling2D(pool_size=(2, 2))(x)
         x = tf.keras.layers.Conv2D(
-            filters=16, kernel_size=(5, 5), activation='sigmoid'
+            filters=16, kernel_size=(5, 5), activation="sigmoid"
         )(x)
         x = tf.keras.layers.AveragePooling2D(pool_size=(2, 2))(x)
         x = tf.keras.layers.Flatten()(x)
         return x
 
     def _classifier(self, x):
-        x = tf.keras.layers.Dense(units=120, activation='sigmoid')(x)
-        x = tf.keras.layers.Dense(units=84, activation='sigmoid')(x)
-        x = tf.keras.layers.Dense(units=10, activation='softmax', name="softmax")(x)
+        x = tf.keras.layers.Dense(units=120, activation="sigmoid")(x)
+        x = tf.keras.layers.Dense(units=84, activation="sigmoid")(x)
+        x = tf.keras.layers.Dense(units=10, activation="softmax", name="softmax")(x)
         return x
 
     def build(self, input_shape):
@@ -35,7 +35,9 @@ class LeNet(tf.keras.Model):
         x = self._classifier(x)
         return x
 
-if __name__ == "__main__":
+
+if __name__ == "__main__": 
     model = LeNet()
     model = model.build((28, 28, 1))
     model.summary()
+
